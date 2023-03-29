f5728540b184b82ba9ce942a9e3b8d7c4eef6e31
Andrey Kurinsky <88144168+dcask@users.noreply.github.com>
2023-03-29 15:42:07 +0300

    Update platform230-install-ubuntu.sh
    2.16 composer

--- platform230-install-ubuntu.sh
+++ platform230-install-ubuntu.sh
@@ -31,7 +31,7 @@ else
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	apt-get update
-	apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+	apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin=2.16.0-1~ubuntu.22.04~jammy -y
 	groupadd docker
 	usermod -aG docker $USER
 	systemctl start docker
