87f546e44ed0360f38934babfa11e1ab1d4d8d2e
nmmkn <64001157+nmmkn@users.noreply.github.com>
2023-04-03 11:53:21 +0300

    Update vp-astra-docker-install.sh

--- astra/vp-astra-docker-install.sh
+++ astra/vp-astra-docker-install.sh
@@ -5,7 +5,7 @@ CONTAINERD_VER=containerd.io_1.6.9-1_amd64.deb
 DOCKER_CE_VER=docker-ce_23.0.1-1~debian.10~buster_amd64.deb
 DOCKER_CE_CLI_VER=docker-ce-cli_23.0.1-1~debian.10~buster_amd64.deb
 DOCKER_BUILDX_VER=docker-buildx-plugin_0.10.2-1~debian.10~buster_amd64.deb
-DOCKER_COMPOSE_VER=docker-compose-plugin_2.17.2-1~debian.10~buster_amd64.deb
+DOCKER_COMPOSE_VER=docker-compose-plugin_2.16.0-1~debian.10~buster_amd64.deb
 CURR_DIR=$( pwd)
 USER=$( echo $USER)
 
