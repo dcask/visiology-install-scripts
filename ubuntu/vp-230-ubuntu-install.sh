e4d072955e6bf0a076bba8e19d28168b083b4df0
nmmkn <64001157+nmmkn@users.noreply.github.com>
2023-04-13 14:26:35 +0300

    Update vp-230-ubuntu-install.sh

--- ubuntu/vp-230-ubuntu-install.sh
+++ ubuntu/vp-230-ubuntu-install.sh
@@ -26,7 +26,7 @@ then
     echo -e "\e[42mDocker установлени\e[0m"
 else
 	echo -e "\e[42mУстановка Docker+Compose\e[0m"
-	apt-get install ca-certificates curl gnupg lsb-release -ydocker 
+	apt-get install ca-certificates curl gnupg lsb-release -y
 	mkdir -p /etc/apt/keyrings
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
