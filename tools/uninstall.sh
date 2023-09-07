c9d33c8b4e9015e6da0923e7624783cdf7410e3d
Andrey Kurinsky <88144168+dcask@users.noreply.github.com>
2023-09-07 17:00:13 +0300

    Create uninstall.sh

---
+++ tools/uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+deadcontainers=$(docker ps -a --format "{{.ID}}" | grep Exited)
+if [[ -n ${deadcontainers} ]]; then
+   docker container rm ${deadcontainers}
+fi
+
+killvolumes=$(docker volume ls --format "{{.Name}}" | grep visiology)
+if [[ -n ${killvolumes} ]];  then
+  docker volume ${killvolumes} rm  -f
+fi
+
+killsecrets=$(docker secret ls --format "{{.Name}}" -q)
+if [[ -n ${killsecrets} ]]; then
+  docker secret rm ${killsecrets}
+fi
+
+killconfigs=$(docker config ls --format "{{.Name}}" -q)
+if [[ -n ${killconfigs} ]]; then
+  docker config rm ${killconfigs}
+fi
+killimages=$(docker images --filter=reference='cr.yandex/crpe1mi33uplrq7coc9d/visiology/release/*' --format "{{ .Repository }}:{{ .Tag }}")
+if [[ -n ${killimages} ]]; then
+  docker rmi ${killimages}
+fi
+
+if [ -d /docker-volume ]; then
+  rm -r /docker-volume
+fi
+
+if [ -d /var/lib/visiology ]; then
+  rm -r /var/lib/visiology
+fi
