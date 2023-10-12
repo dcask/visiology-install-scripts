cf6c29e1147ca828de2bb853410619a68f42509d
Andrey Kurinsky <88144168+dcask@users.noreply.github.com>
2023-10-12 11:26:53 +0300

    Update docker-install.sh

--- ubuntu/docker-install.sh
+++ ubuntu/docker-install.sh
@@ -21,13 +21,20 @@ if docker -v docker &> /dev/null
 then
     echo -e "\e[42mDocker установлени\e[0m"
 else
-	echo -e "\e[42mУстановка Docker+Compose\e[0m"
-	apt-get install ca-certificates curl gnupg lsb-release -ydocker 
-	mkdir -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	# Add Docker's official GPG key:
 	apt-get update
-	apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+	apt-get install ca-certificates curl gnupg -y
+	install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	chmod a+r /etc/apt/keyrings/docker.gpg
+	
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+	  tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt-get update
+	apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 	groupadd docker
 	usermod -aG docker $USER
 	systemctl start docker
